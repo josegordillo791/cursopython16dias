30e95c7f3408a3cc10d528e4328d0aba94500f00
José Gordillo Panighini <63110718+josegordillo791@users.noreply.github.com>
2022-12-20 21:42:07 -0300

    Delete proyecto_dia2.py

--- proyecto_dia2.py
+++
@@ -1,9 +0,0 @@
-"""pedir al uysuario nombre y ventas totales ,mostrar el 13 % de comision
-para saber cuanto gano extra"""
-
-nombre=input("Ingresa tu nombre")
-ventas=input("Ingresa tus ventas totales")
-comision=float(ventas)**0.13
-ventasTotales=round(float(ventas)+comision,2)
-
-print(f"${ventasTotales} usd")
